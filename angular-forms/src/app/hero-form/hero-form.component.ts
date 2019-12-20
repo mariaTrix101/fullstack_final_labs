077f26b7d6bd9978b051e6a69d30330479ac63cc
mariaTrix101 <tnjsoares@gmail.co>
2019-12-20 02:57:18 -0500

    Committed Local Labs

---
+++ angular-forms/src/app/hero-form/hero-form.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+import { Hero }    from '../hero';
+
+@Component({
+  selector: 'app-hero-form',
+  templateUrl: './hero-form.component.html',
+  styleUrls: ['./hero-form.component.css']
+})
+export class HeroFormComponent {
+
+  powers = ['Invisibility', 'Super Strength',
+            'Flexibility', 'Super Speed'];
+
+  model = new Hero(1, 'Dr Fantastic', this.powers[0], 'Bob Dole');
+
+  submitted = false;
+
+  onSubmit() { this.submitted = true; }
+
+  // TODO: Remove this when we're done
+  get diagnostic() { return JSON.stringify(this.model); }
+
+  newHero() {
+    this.model = new Hero(2, '', '');
+  }
+}
---
+++ lab10/app.js
@@ -0,0 +1,68 @@
+const Express = require("express");
+const Mongoose = require("mongoose");
+const BodyParser = require("body-parser");
+
+var app = Express();
+
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({ extended: true }));
+
+app.listen(3000, () => {
+    console.log("Listening at localhost:3000...");
+});
+
+
+Mongoose.connect("mongodb://localhost/lab10");
+
+const PersonModel = Mongoose.model("person", {
+    firstname: String,
+    lastname: String
+});
+
+app.post("/person", async (request, response) => {
+    try {
+        var person = new PersonModel(request.body);
+        var result = await person.save();
+        response.send(result);
+    } catch (error) {
+        response.status(500).send(error);
+    }
+});
+
+app.get("/people", async (request, response) => {
+    try {
+        var result = await PersonModel.find().exec();
+        response.send(result);
+    } catch (error) {
+        response.status(500).send(error);
+    }
+});
+
+app.get("/person/:id", async (request, response) => {
+    try {
+        var person = await PersonModel.findById(request.params.id).exec();
+        response.send(person);
+    } catch (error) {
+        response.status(500).send(error);
+    }
+});
+
+app.put("/person/:id", async (request, response) => {
+    try {
+        var person = await PersonModel.findById(request.params.id).exec();
+        person.set(request.body);
+        var result = await person.save();
+        response.send(result);
+    } catch (error) {
+        response.status(500).send(error);
+    }
+});
+
+app.delete("/person/:id", async (request, response) => {
+    try {
+        var result = await PersonModel.deleteOne({ _id: request.params.id }).exec();
+        response.send(result);
+    } catch (error) {
+        response.status(500).send(error);
+    }
+});
